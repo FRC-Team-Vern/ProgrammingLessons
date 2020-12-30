c809e281636297ca24b4bef09a3bf5ffdd4b6867
Randy Steiner <steinra2002@yahoo.com>
2020-12-29 19:51:34 -0700

    Adding lesson files

--- src/vern/team5461/programminglessons/lesson1/HelloWorld.java
+++ src/vern/team5461/programminglessons/lesson1/HelloWorld.java
@@ -2,9 +2,10 @@ package vern.team5461.programminglessons.lesson1;
 
 public class HelloWorld {
 
-    static public void main( String [] args ) {
+    static public void main( String[] args ) {
 
         System.out.println( "Hello World!" );
+        System.out.println("My name is Randy!");
 
     }
 }
